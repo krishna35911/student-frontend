fe6f5ca1aba798b53633b33228b11b083cde9e81
krishna35911 <krishnakp35911@gmail.com>
2024-10-02 15:47:15 +0530

    add file

--- src/components/Add.jsx
+++ src/components/Add.jsx
@@ -75,9 +75,9 @@ function Add() {
   </div>
 
   <div className='flex items-center justify-between'>
-      <button type="submit" className="text-white bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5" onClick={submit}>Submit</button>
+      <button type="submit" className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5" onClick={submit}>Submit</button>
     
-      <Link to={'/'}><button className="text-white bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5">Back</button></Link>
+      <Link to={'/'}><button className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5">Back</button></Link>
   </div>
 </form>
   )
--- src/components/EditStudent.jsx
+++ src/components/EditStudent.jsx
@@ -76,9 +76,9 @@ function EditStudent() {
   </div>
 
   <div className='flex items-center justify-between'>
-      <button type="submit" className="text-white bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5" onClick={handleedit}>Update</button>
+      <button type="submit" className="text-white bg-blue-700 font-medium rounded-lg text-sm  px-5 py-2.5" onClick={handleedit}>Update</button>
     
-      <Link to={'/'}><button className="text-white bg-blue-700 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5">Back</button></Link>
+      <Link to={'/'}><button className="text-white bg-blue-700 font-medium rounded-lg text-sm px-5 py-2.5">Back</button></Link>
   </div>
 </form>
 
