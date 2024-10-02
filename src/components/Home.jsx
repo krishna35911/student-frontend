861c444755c7fec5d3bbbd7a5497df47888e094f
krishna35911 <krishnakp35911@gmail.com>
2024-10-02 15:37:03 +0530

    filename changed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -99,7 +99,7 @@ function Home() {
 </div>
 
 <div className='text-center mt-9'>
-    <Link to={'/addstudent'}><button className="bg-green-700 border-0 py-1 px-5 rounded-md text-white">Add student</button></Link>
+    <Link to={'/addstudent'}><button className="bg-green-700 border-0 py-2 px-5 rounded-md text-white">Add student</button></Link>
 </div>
 
     </>
